446825c74f174a1314c795b4cb500182c3035de7
Zeioth <zeioth@hotmail.com>
2023-07-01 14:06:16 +0200

    Initial commit

---
+++ hello_world.py
@@ -0,0 +1,8 @@
+def hello_world(name):
+    if name == "Fabiola":
+        print("Que dia mas bonito hace")
+    else:
+        print("Hello world " + name)
+
+
+hello_world("Manolo")
---
+++ mi_programa_random.py
@@ -0,0 +1,5 @@
+import random
+
+# Generar un número aleatorio entre 1 y 10
+numero_aleatorio = random.randint(1, 10)
+print(numero_aleatorio)
